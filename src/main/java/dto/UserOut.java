93f7f98359fe71f96fb96b942eda7de343b7db3c
Tatyana Pushkarskaya <t.pushkarskaya@inbox.ru>
2021-11-05 02:50:32 +0300

    Добавление тест-кейсов

---
+++ src/main/java/dto/UserOut.java
@@ -0,0 +1,19 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonSerialize
+
+public class UserOut {
+    private Long code;
+    private String message;
+    private String type;
+}
--- src/test/java/User/createUser/CreateUserTest.java
+++ src/test/java/User/createUser/CreateUserTest.java
@@ -1,6 +1,10 @@
 package User.createUser;
 
+import dto.UserOut;
 import dto.User;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import services.UserApi;
 import java.util.logging.Logger;
@@ -11,9 +15,9 @@ public class CreateUserTest {
     Logger logger;
 
     public void checkCreateUser(String userName) {// Создание пользователя с заданным ником
-        User user = User.builder()
-                .userStatus(10l)
-                .email("kkk")
+        User user = User.builder()                // Валидация ответа statusCode=200
+                .userStatus(10l)                  //                  type="unknown"
+                .email("kkk")                     //                  message="203"
                 .id(203l)
                 .firstName("Geits")
                 .lastName("Bill")
@@ -25,17 +29,92 @@ public class CreateUserTest {
                 .then()
                 .log().all()
                 .statusCode(200)
-                .time(lessThan(5000l))
                 .body("type", equalTo("unknown"))
-                .body("message", comparesEqualTo("203"));
+                .body("message", comparesEqualTo(203));
     }
 
-    public void checkUserByName(String userName) { //Поиск пользователя по имени
+    public void checkUserByName(String userName) { //Получение пользователя по имени
         userApi.getUserByName(userName)
                 .then()
                 .log().all()
                 .statusCode(200)
                 .body("username", equalTo(userName));
+    }
+
+    @Test
+    public void checkCreateUserWithAssert() {// Создание пользователя, проверка c помощью ассерта,
+        User user = User.builder()           //что ответ от сервера 200
+                .userStatus(10l)
+                .email("kkk")
+                .id(203l)
+                .firstName("Lara")
+                .lastName("Fabian")
+                .password("password")
+                .username("userForCheckTime")
+                .phone("8-976-098-87-09")
+                .build();
+        Response response = userApi.getUser(user);
+        UserOut res = response.then().extract().body().as(UserOut.class);//Преобразование ответа сервера к классу CreateUserOut
+        Assert.assertEquals("200", res.getCode().toString());
+    }
+
+@Test
+    public void checkCreateUserTime(){// Создание пользователя
+    User user = User.builder()                // Проверка, что время ответа сервера при создании
+            .userStatus(10l)                  // пользователя не превышает 4 мс
+            .email("kkk")
+            .id(203l)
+            .firstName("Ayyyyy")
+            .lastName("Woooo")
+            .password("password1")
+            .username("Varvara")
+            .phone("phone")
+            .build();
+            userApi.getUser(user)
+                    .then()
+                    .time(lessThan(4000L));
+
+}
+@Test
+public void checkCreateUserSchema() {// Создание пользователя
+        User user = User.builder()                // Валидация ответа по json схеме
+                .userStatus(10l)                  // Проверка наличия обязательных полей ответа
+                .email("kkk")                     //Позитивный тест
+                .id(203l)
+                .firstName("Geits")
+                .lastName("Bill")
+                .password("password")
+                .username("Voyager")
+                .phone("phone")
+                .build();
+        userApi.getUser(user)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("unknown"))
+                .log().all()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema/CreateUser.json"));
+
+    }
+
+    @Test
+    public void checkCreateUserSchemaNegative() {// Создание пользователя
+        User user = User.builder()                // Валидация ответа по json схеме
+                .userStatus(10l)                  // Проверка наличия обязательных полей ответа
+                .email("kkk")                     //Негативный тест, вставили несуществующее в ответе поле typenone
+                .id(203l)
+                .firstName("Geits")
+                .lastName("Bill")
+                .password("password")
+                .username("Voyager")
+                .phone("phone")
+                .build();
+        userApi.getUser(user)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("unknown"))
+                .log().all()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema/CreateUserNegative.json"));
+
     }
 
     //Тесты на создание пользователя
@@ -45,8 +124,9 @@ public class CreateUserTest {
     }
 
     @Test
-    public void testCheckCreateUserMoon() {//Тест Создание и проверка успешного создания пользователя с ником Moon
+    public void testCheckCreateUserMoon() {//Тест Сна валидность json схемы
         checkCreateUser("Moon");
+
     }
 
     //Тесты на получение пользователя
